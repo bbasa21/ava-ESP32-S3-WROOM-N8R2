05f9d183c1973f0896213ae46f2842b03f2c80ab
bbasa21 <alipourgholi236@gmail.com>
2026-09-25 16:33:06 +0330

    Add firmware build metadata header

---
+++ include/Ava_BuildInfo.h
@@ -0,0 +1,11 @@
+#ifndef AVA_BUILD_INFO_H
+#define AVA_BUILD_INFO_H
+
+// Local/manual builds use 0.
+// The release workflow replaces this header with the real build number.
+
+#define AVA_BUILD_NUMBER 0
+#define AVA_BUILD_VERSION "0.0.0"
+#define AVA_BUILD_COMMIT ""
+
+#endif
