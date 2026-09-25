6158e37f276afd80f2639c197e48d6089716b858
bbasa21 <alipourgholi236@gmail.com>
2026-09-25 16:33:04 +0330

    Add AVA OTA interface

---
+++ include/Ava_OTA.h
@@ -0,0 +1,12 @@
+#ifndef AVA_OTA_H
+#define AVA_OTA_H
+
+#include <Arduino.h>
+
+// ==================================================
+// AVA OTA
+// ==================================================
+
+bool avaOTAUpdate();
+
+#endif
